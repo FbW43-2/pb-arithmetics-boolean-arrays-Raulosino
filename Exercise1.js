945d45932c4cd24bf198995b499c2e170edd7df0
Raulosino <eduarteraul@gmail.com>
2020-12-04 13:15:56 +0100

    Solutions 1

---
+++ Exercise1.js
@@ -0,0 +1,61 @@
+// Step 1
+var snackServings = 15;
+var guests = 10;
+var operation;
+
+// Step 2
+console.log("Step 2");
+operation = snackServings + guests;
+console.log(operation);
+operation = 0;
+
+// Step 3
+console.log("Step 3");
+operation = guests - snackServings;
+operation = operation + (snackServings - guests);
+console.log(operation);
+operation = 0;
+
+// Step 4
+console.log("Step 4");
+operation = snackServings * guests;
+console.log(operation);
+operation = 0;
+
+// Step 5
+console.log("Step 5");
+operation = snackServings / guests;
+operation = 0;
+
+// Step 6
+console.log("Step 6");
+var drinks = 10;
+operation = snackServings * guests;
+operation = operation / drinks;
+var resultOne = operation;
+console.log(resultOne);
+operation = 0;
+
+// Step 7
+console.log("Step 7");
+var monkeys = 15;
+var bananas = 9;
+operation = 15 / 9;
+console.log(operation);
+operation = 0;
+
+// Step 8
+console.log("Step 8");
+var monkeyFights = 20;
+operation = monkeys + bananas;
+operation = operation * monkeyFights;
+var resultTwo = operation;
+console.log(resultTwo);
+operation = 0;
+
+// Step 9
+console.log("Step 9");
+monkeys++;
+console.log("Monkeys: " + monkeys)
+
+// Step
