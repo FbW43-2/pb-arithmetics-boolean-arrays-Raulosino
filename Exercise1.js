19315cbb755204ed9875e69f5e643b0b893adff3
Raulosino <43993330+Raulosino@users.noreply.github.com>
2020-12-08 20:25:06 +0100

    Update Exercise1.js

--- Exercise1.js
+++ Exercise1.js
@@ -1,80 +1,57 @@
 // Step 1
-console.log("Step 1:");
 var snackServings = 15;
 var guests = 10;
-var operation;
 
 // Step 2
 console.log("Step 2:");
-operation = snackServings + guests;
-console.log(operation);
-operation = 0;
+console.log(snackServings + guests)
 
 // Step 3
-console.log("Step 3:");
-operation = guests - snackServings;
-operation = operation + (snackServings - guests);
-console.log(operation);
-operation = 0;
+console.log("\nStep 3:");
+console.log(guests - snackServings);
+console.log(snackServings - guests);
 
 // Step 4
-console.log("Step 4:");
-operation = snackServings * guests;
-console.log(operation);
-operation = 0;
+console.log("\nStep 4:");
+console.log(snackServings * guests);
 
 // Step 5
-console.log("Step 5:");
-operation = snackServings / guests;
-operation = 0;
+console.log("\nStep 5:");
+console.log(snackServings / guests);
 
 // Step 6
-console.log("Step 6:");
+console.log("\nStep 6:");
 var drinks = 10;
-operation = snackServings * guests;
-operation = operation / drinks;
-var resultOne = operation;
+var resultOne = (snackServings * guests) / drinks;
 console.log(resultOne);
-operation = 0;
 
 // Step 7
-console.log("Step 7:");
+console.log("\nStep 7:");
 var monkeys = 15;
 var bananas = 9;
-operation = 15 / 9;
-console.log(operation);
-operation = 0;
+console.log(monkeys / bananas);
 
 // Step 8
-console.log("Step 8:");
+console.log("\nStep 8:");
 var monkeyFights = 20;
-operation = monkeys + bananas;
-operation = operation * monkeyFights;
-var resultTwo = operation;
+var resultTwo = (monkeys + bananas) * monkeyFights;
 console.log(resultTwo);
-operation = 0;
 
 // Step 9
-console.log("Step 9:");
+console.log("\nStep 9:");
 monkeys++;
 console.log("Monkeys: " + monkeys)
 
 // Step 10
-console.log("Step 10:");
+console.log("\nStep 10:");
 bananas--;
 console.log("Bananas: " + bananas);
 
 // Step 11
-console.log("Step 11:");
-operation = monkeys - bananas;
-var happiness = operation;
-operation = 0;
-operation = happiness + monkeyFights;
-console.log("happiness + monkeyFights = " + operation);
-operation = 0;
+console.log("\nStep 11:");
+var happiness = monkeys - bananas;
+console.log("happiness + monkeyFights = " + `${happiness + monkeyFights}`);
 
 // Step 12
-console.log("Step 12:");
-operation = resultOne % resultTwo;
-console.log(operation);
-operation = 0;
+console.log("\nStep 12:");
+console.log(resultOne % resultTwo);
